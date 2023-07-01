f2a0eb6b824d49fd87e53d703c7ed459aee5630e
ProdipKumarShil <prodipkrishna01@gmail.com>
2023-07-01 20:14:49 +0600

    added parallex tilt effect

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,6 +1,14 @@
 import html from '../assets/logos/html.png'
+import css from '../assets/logos/css.png'
+import bootstrap from '../assets/logos/bootstrap.png'
+import express from '../assets/logos/express.png'
+import firebase from '../assets/logos/firebase.png'
+import js from '../assets/logos/js.png'
+import mongodb from '../assets/logos/mongodb.png'
+import nodejs from '../assets/logos/nodejs.png'
+import react from '../assets/logos/react.png'
+import tailwindcss from '../assets/logos/tailwindcss.png'
 import SkillCard from '../shared/SkillCard';
-import Tilt from "react-parallax-tilt";
 
 const Skills = () => {
   return (
@@ -9,14 +17,15 @@ const Skills = () => {
       <div className="flex flex-wrap px-8 gap-4 justify-center py-6">
         {/* skill card */}
         <SkillCard img={html} text="html"></SkillCard>
-        <SkillCard img={html} text="html"></SkillCard>
-        <SkillCard img={html} text="html"></SkillCard>
-        <SkillCard img={html} text="html"></SkillCard>
-        <SkillCard img={html} text="html"></SkillCard>
-        <SkillCard img={html} text="html"></SkillCard>
-        <SkillCard img={html} text="html"></SkillCard>
-        
-        
+        <SkillCard img={css} text="css"></SkillCard>
+        <SkillCard img={js} text="javascript"></SkillCard>
+        <SkillCard img={bootstrap} text="bootstrap"></SkillCard>
+        <SkillCard img={tailwindcss} text="tailwind"></SkillCard>
+        <SkillCard img={react} text="react"></SkillCard>
+        <SkillCard img={nodejs} text="node js"></SkillCard>
+        <SkillCard img={express} text="express js"></SkillCard>
+        <SkillCard img={firebase} text="firebase"></SkillCard>
+        <SkillCard img={mongodb} text="mongodb"></SkillCard>
         
       </div>
     </div>
