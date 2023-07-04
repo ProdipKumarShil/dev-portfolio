ca3809f6d103308d1458761c06f3bd8c3f39f46e
ProdipKumarShil <prodipkrishna01@gmail.com>
2023-07-05 00:30:05 +0600

    added drawer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,10 +27,10 @@ const Navbar = () => {
           {navItems}
         </ul>
       </div>
-      <div onClick={handleMenu} className="flex-none md:hidden cursor-pointer active:scale-95 hover:bg-zinc-300 rounded-lg">
+      {/* <div onClick={handleMenu} className="flex-none md:hidden cursor-pointer active:scale-95 hover:bg-zinc-300 rounded-lg">
         {menu ? <CgMenuRightAlt className="scale-150 m-4" /> : <CgMenuRight className="scale-150 m-4" />}
-      </div>
-      <div className="relative">
+      </div> */}
+      {/* <div className="relative">
         {
           !menu && <div className="absolute md:hidden -right-2 top-8 p-4 rounded-lg bg-green-500 transition ease-in-out delay-150 ">
           <ul className="menu menu-horizontal px-1 md:hidden inline-block">
@@ -38,6 +38,20 @@ const Navbar = () => {
           </ul>
         </div>
       }
+      </div> */}
+      <div className="drawer drawer-end md:hidden justify-end z-10">
+        <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content">
+          {/* Page content here */}
+          <label htmlFor="my-drawer-4" className="drawer-button btn  "> <CgMenuRightAlt className="text-xl"/> </label>
+        </div>
+        <div className="drawer-side">
+          <label htmlFor="my-drawer-4" className="drawer-overlay"></label>
+          <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content">
+            {/* Sidebar content here */}
+            {navItems}
+          </ul>
+        </div>
       </div>
       
     </div>
